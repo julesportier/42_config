47b7f2967decb3618826e26df42164e161b2bbf4
Jules Portier <juportie@z2r9p1.42lyon.fr>
2024-12-04 13:11:37 +0100

    ADD(nvim): nvim-treesitter plugin

---
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,21 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function () 
+		local configs = require("nvim-treesitter.configs")
+
+		configs.setup({
+			ensure_installed = {
+				"lua", "vim", "vimdoc",
+				"gitcommit", "gitignore", "git_config", "git_rebase", "diff",
+				"c", "make",
+				"bash",
+				"markdown", "markdown_inline"
+			},
+			auto_install = true,
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },  
+		})
+	end
+ }
