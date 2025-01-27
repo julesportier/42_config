1fb5801abc7ef4e06cb739729a6c292ff63ce0a6
Jules Portier <juportie@z2r9p1.42lyon.fr>
2025-01-27 15:36:16 +0100

    tweak(nvim): persistent signcolumn to avoid screen shaking with lsp

--- nvim/init.lua
+++ nvim/init.lua
@@ -58,6 +58,28 @@ vim.cmd.colorscheme "catppuccin"
 ----------------
 -- LSP CONFIG --
 ----------------
+vim.opt.signcolumn = 'yes'
+--vim.api.nvim_create_autocmd('LspAttach', {
+--	desc = 'LSP actions',
+--	callback = function(event)
+--		local opts = {buffer = event.buf}
+--
+--		-- Defaults keybindings --
+--		vim.keymap.set('n', '<C-]>', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+--		vim.keymap.set({'n', 'x'}, 'gq', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+--		vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+--		vim.keymap.set('n', 'grn', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+--		vim.keymap.set('n', 'gra', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+--		vim.keymap.set('n', 'grr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+--		vim.keymap.set('n', 'gri', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+--		vim.keymap.set('n', 'gO', '<cmd>lua vim.lsp.buf.document_symbol()<cr>', opts)
+--		vim.keymap.set('i', '<C-s>', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+--		--------------------------
+--		--vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+--		--vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+--		--vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+--	end,
+--})
 
 
 ---------------------
