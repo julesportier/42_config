f6c32565c6289e0ada9588177005ae9527b319ec
jules <142106821+julesportier@users.noreply.github.com>
2025-03-18 15:25:03 +0100

    feat(nvim): a catpuccin latte theme with high bg/fg contrasts

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -8,7 +8,7 @@ return {
 		-- 	light = "latte",
 		-- 	dark = "mocha",
 		-- },
-		transparent_background = true,
+		transparent_background = false,
 		styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
 			-- comments = { "italic" },
 			-- conditionals = { "italic" },
@@ -54,15 +54,45 @@ return {
 				mantle = "#141620",
 				crust = "#0e0f16",
 			},
+			-- latte = { --more contrast
+			-- 	text = "#3c3e52",
+			-- 	peach = "#f45a01",
+			-- 	green = "#3a9127",
+			-- 	blue = "#0b59f4",
+			-- 	lavender = "#5f77fa",
+			-- 	--base = "#ffffff", --background
+			-- 	--mantle = "#ffffff", --status bar
+			-- 	--crust = "#ffffff",
+			-- },
 			latte = { --more contrast
-				text = "#3c3e52",
-				peach = "#f45a01",
-				green = "#3a9127",
-				blue = "#0b59f4",
-				lavender = "#5f77fa",
-				--base = "#ffffff", --background
-				--mantle = "#ffffff", --status bar
-				--crust = "#ffffff",
+				-- HSL's Lightness - 10%
+				rosewater = "#d26851",
+				flamingo = "#d35050",
+				pink = "#e34aba",
+				mauve = "#6d12e2",
+				red = "#a20b2c",
+				maroon = "#dc1e2e",
+				peach = "#d54f01",
+				yellow = "#b07017",
+				green = "#307920",
+				teal = "#179299",
+				sky = "#0382b5",
+				sapphire = "#187a8b",
+				blue = "#0a4ed6",
+				lavender = "#5d72ea", -- S-20,L-8
+				text = "#36384a",
+				subtext1 = "#454759",
+				subtext0 = "#555768",
+			 	overlay2 = "#636679",
+				overlay1 = "#707489",
+				overlay0 = "#808599",
+				-- colors above not modified
+				surface2 = "#acb0be",
+				surface1 = "#bcc0cc", -- selection bg, line numbers
+				surface0 = "#ccd0da",
+				base = "#fcfcfd", -- L+4
+				mantle = "#e9ecf1",
+				crust = "#dce0e8",
 			},
 		},
 	},
